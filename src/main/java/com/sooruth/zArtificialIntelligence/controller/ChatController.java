da7186d0bcabc0ee4dc92a1af3a7f046584e166a
zulfekar <17052721+ZS25@users.noreply.github.com>
2025-01-15 03:52:04 -0500

    Added support for streaming of responses.  No longer need to wait for the whole message.

--- src/main/java/com/sooruth/zArtificialIntelligence/controller/ChatController.java
+++ src/main/java/com/sooruth/zArtificialIntelligence/controller/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -32,6 +33,14 @@ public class ChatController {
         return chatClient.prompt()
                 .user(message)
                 .call()
+                .content();
+    }
+
+    @GetMapping("/chatstream")
+    public Flux<String> chatWithStream(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .stream()
                 .content();
     }
 

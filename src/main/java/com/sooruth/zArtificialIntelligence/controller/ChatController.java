a55e85ec1c05de3f916862881e86e3f75d36f9f7
zulfekar <17052721+ZS25@users.noreply.github.com>
2025-02-25 01:31:11 -0500

    Added system role to respond in uppercase and topic for book endpoint returning list of book records.

--- src/main/java/com/sooruth/zArtificialIntelligence/controller/ChatController.java
+++ src/main/java/com/sooruth/zArtificialIntelligence/controller/ChatController.java
@@ -5,10 +5,13 @@ import com.sooruth.zArtificialIntelligence.record.BookRecord;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 public class ChatController {
@@ -17,6 +20,7 @@ public class ChatController {
 
     public ChatController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder
+                .defaultSystem("respond with uppercase")
                 .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
                 .build();
     }
@@ -30,11 +34,19 @@ public class ChatController {
     }
 
     @GetMapping("/book")
-    public BookRecord book() {
+    public BookRecord book(@RequestParam String topic) {
         return chatClient.prompt()
-                .user("Give me a book recommendation on Java.  Limit the summary to 100 words.")
+                .user(u -> u.text("Give me a book recommendation on {topic}.  Limit the summary to 100 words.").param("topic", topic))
                 .call()
                 .entity(BookRecord.class);
+    }
+
+    @GetMapping("/books")
+    public List<BookRecord> books(@RequestParam String topic) {
+        return chatClient.prompt()
+                .user(u -> u.text("Give me two books recommendation on {topic}.  Limit the summary to 100 words.").param("topic", topic))
+                .call()
+                .entity(new ParameterizedTypeReference<>() {});
     }
 
     @GetMapping("/chat")
